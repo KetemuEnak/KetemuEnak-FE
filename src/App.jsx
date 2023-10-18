35c1c97a8e03c4eb7a0819e46a8940a37963ea1c
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2023-10-18 19:41:28 +0700

    setup react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+const Hello = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
 export default App;
