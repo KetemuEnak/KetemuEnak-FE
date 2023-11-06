4779e50ea6187e0712055566e7b7d7883dbacd09
allfitra <alfitrafadjri00@gmail.com>
2023-11-06 18:48:47 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/components/home-content.jsx", "./src/components/detail-content.jsx", "./src/components/profile-content.jsx", "./src/pages/home-page.jsx", "./src/pages/detail-page.jsx", "./src/pages/profile-page.jsx"],
+  content: [
+    "./src/components/HomePage/home-content.jsx",
+    "./src/components/DetailEvent/detail-content.jsx",
+    "./src/components/Profile/profile-eo.jsx",
+    "./src/components/Profile/profile-seller.jsx",
+    "./src/pages/HomePage/index.jsx",
+    "./src/pages/DetailPage/index.jsx",
+    "./src/pages/ProfilePage/index.jsx",
+  ],
   theme: {
     extend: {},
   },
