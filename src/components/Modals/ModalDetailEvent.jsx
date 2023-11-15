4805e423a0de33b041f0b8a3e132197c6bc31f79
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2023-11-15 22:17:14 +0700

    finish fetch seller's page

--- src/components/Modals/ModalDetailEvent.jsx
+++ src/components/Modals/ModalDetailEvent.jsx
@@ -37,9 +37,13 @@ const ModalDetailEvent = ({
             <Tooltip content="Lokasi" placement="bottom">
               <div className="flex gap-x-1 items-center content-center">
                 <IoLocationOutline />
-                <p className="font-normal text-gray-700">
-                  {location} {city}
-                </p>
+                <a
+                  href={location}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-normal text-gray-700">
+                  {city}
+                </a>
               </div>
             </Tooltip>
           </div>
